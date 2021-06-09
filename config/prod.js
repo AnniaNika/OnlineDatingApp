1aa827b21cace1dad2f6e59aff17d2d224c51d6a
AnniaNika <ToniaNika@192.168.1.34>
2021-06-09 20:39:47 +0300

    fb update

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,10 @@
 module.exports = {
-    
+
     MongoDB: process.env.MONGO_DB,
-    FacebookAppId: '165619915432340',
-    FacebookAppSecret: 'cb8635bb329549b6791ba096f6a59d3c',
+    //FacebookAppId: '165619915432340',
+    FacebookAppId: process.env.FB_APP_ID,
+    //FacebookAppSecret: 'cb8635bb329549b6791ba096f6a59d3c',
+    FacebookAppSecret: process.env.FB_APP_SECRET,
     GoogleClientID: '168669449814-ldmdotgllsuo6bugb2tpm4l0sgi71elf.apps.googleusercontent.com',
     GoogleClientSecret: 'HMFvx_Ry-U4j_By9LGsw4bDa' 
 };
