e58b585db7b7fe53028ea917e871db473589a897
AnniaNika <ToniaNika@192.168.1.34>
2021-06-09 22:15:41 +0300

    google cl id

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,8 @@ module.exports = {
     FacebookAppId: process.env.FB_APP_ID,
     //FacebookAppSecret: 'cb8635bb329549b6791ba096f6a59d3c',
     FacebookAppSecret: process.env.FB_APP_SECRET,
-    GoogleClientID: '168669449814-ldmdotgllsuo6bugb2tpm4l0sgi71elf.apps.googleusercontent.com',
-    GoogleClientSecret: 'HMFvx_Ry-U4j_By9LGsw4bDa' 
+    //GoogleClientID: '168669449814-ldmdotgllsuo6bugb2tpm4l0sgi71elf.apps.googleusercontent.com',
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    //GoogleClientSecret: 'HMFvx_Ry-U4j_By9LGsw4bDa' 
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
