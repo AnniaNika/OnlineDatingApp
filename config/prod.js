a4cb6198eb03bb31a146e328ba1d4a69548afb28
AnniaNika <ToniaNika@192.168.1.34>
2021-06-09 18:27:43 +0300

    heroku changes

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://onlinedatingapp:12341234@onlinedatingapp.rkep6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    FacebookAppId: '165619915432340',
+    FacebookAppSecret: 'cb8635bb329549b6791ba096f6a59d3c',
+    GoogleClientID: '168669449814-ldmdotgllsuo6bugb2tpm4l0sgi71elf.apps.googleusercontent.com',
+    GoogleClientSecret: 'HMFvx_Ry-U4j_By9LGsw4bDa' 
+};
