014278c1fbf574f373d7b7bb97a85e984b4dd4ca
AnniaNika <ToniaNika@192.168.1.34>
2021-06-09 20:02:32 +0300

    mongoDB update

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
-    MongoDB: 'mongodb+srv://onlinedatingapp:12341234@onlinedatingapp.rkep6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    //MongoDB: 'mongodb+srv://onlinedatingapp:12341234@onlinedatingapp.rkep6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    MongoDB: process.env.MONGO_DB,
     FacebookAppId: '165619915432340',
     FacebookAppSecret: 'cb8635bb329549b6791ba096f6a59d3c',
     GoogleClientID: '168669449814-ldmdotgllsuo6bugb2tpm4l0sgi71elf.apps.googleusercontent.com',
